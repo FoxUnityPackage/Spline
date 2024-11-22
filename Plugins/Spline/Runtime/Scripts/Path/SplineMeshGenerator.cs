c908e23f25721d4af870ef87e50a7ef1af7bf952
Jonathan Six <jonathan.six@ubisoft.com>
2024-11-22 10:27:32 +0100

    Move asset and fix leak

--- Plugins/Spline/Runtime/Scripts/Path/SplineMeshGenerator.cs
+++ Plugins/Spline/Runtime/Scripts/Path/SplineMeshGenerator.cs
@@ -1,29 +1,24 @@
 using UnityEngine;
 
-public class PathGenerator : MonoBehaviour
+public class SplineMeshGenerator : MonoBehaviour
 {
-    public Spline spline;
-    public MeshFilter meshFilter;
-    [SerializeField] public Vector3 biTangeante = Vector3.back;
-    public float scale = 0.1f;
-    [Min(3)]
-    public int division = 3;
+    public Spline Spline;
+    public MeshFilter MeshFilter;
+    [SerializeField] public Vector3 BiTangeante = Vector3.back;
+    public float Scale = 0.1f;
+    [Min(3)] public int Division = 3;
 
     public void GenerateMesh()
     {
-        Debug.Assert(meshFilter != null, "meshFilter != null");
-        Vector3[] points = spline.MakeSplinePoints(division);
+        Debug.Assert(MeshFilter != null, "meshFilter != null");
+        Vector3[] points = Spline.MakeSplinePoints(Division);
         
         if (points != null)
         {
-            Mesh mesh = new Mesh();
-            mesh.name = "GeneratedMesh";
-            
+            Mesh mesh = MeshFilter.mesh;
             mesh.SetVertices(GenerateVertices(points));
             mesh.SetIndices(GenerateIndices(points), MeshTopology.Triangles, 0);
             mesh.SetUVs(0, GenerateUVs(points));
-
-            meshFilter.mesh = mesh;
         }
     }
 
@@ -33,7 +28,7 @@ public class PathGenerator : MonoBehaviour
         Vector3[] vertices = new Vector3[4 * (points.Length - 1)];
         
         Vector3 tangeante;
-        tangeante = Vector3.Normalize(Vector3.Cross(biTangeante, points[1] - points[0])) * scale;
+        tangeante = Vector3.Normalize(Vector3.Cross(BiTangeante, points[1] - points[0])) * Scale;
         vertices[0] = points[0] + tangeante;
         vertices[1] = points[0] - tangeante;
         
@@ -41,7 +36,7 @@ public class PathGenerator : MonoBehaviour
  
         for (int i = 2; i < vertices.Length - 6; i += 4)
         {
-            tangeante = Vector3.Normalize(Vector3.Cross(biTangeante, points[index + 1] - points[index])) * scale;
+            tangeante = Vector3.Normalize(Vector3.Cross(BiTangeante, points[index + 1] - points[index])) * Scale;
             vertices[i + 0] = points[index] + tangeante;
             vertices[i + 1] = points[index] - tangeante;
             // Second pass for UVs
@@ -50,12 +45,12 @@ public class PathGenerator : MonoBehaviour
             index++;
         }
         
-        tangeante = Vector3.Normalize(Vector3.Cross(biTangeante, points[index + 1] - points[index])) * scale;
+        tangeante = Vector3.Normalize(Vector3.Cross(BiTangeante, points[index + 1] - points[index])) * Scale;
         vertices[vertices.Length - 6] = points[index] + tangeante;
         vertices[vertices.Length - 5] = points[index] - tangeante;
 
         // Use the last direction
-        tangeante = Vector3.Normalize(Vector3.Cross(biTangeante, points[ points.Length - 1] - points[ points.Length - 2])) * scale;
+        tangeante = Vector3.Normalize(Vector3.Cross(BiTangeante, points[ points.Length - 1] - points[ points.Length - 2])) * Scale;
         vertices[vertices.Length - 4] = points[index] + tangeante;
         vertices[vertices.Length - 3] = points[index] - tangeante;
         // Second pass for UVs
@@ -84,18 +79,34 @@ public class PathGenerator : MonoBehaviour
         return indices;
     }
 
-    Vector2[] GenerateUVs(Vector3[] points)
+    Vector2[] GenerateUVs(Vector3[] points, bool uvPerSection = false)
     {
         // Create quad for each point
         Vector2[] uvs = new Vector2[4 * (points.Length - 1)];
-        
-        for (int i = 0; i < uvs.Length; i += 4)
+
+        if (uvPerSection)
         {
-            uvs[i + 0] = new Vector2(0f, 0f);
-            uvs[i + 1] = new Vector2(1f, 0f);
-            uvs[i + 2] = new Vector2(0f, 1f);
-            uvs[i + 3] = new Vector2(1f, 1f);
+            for (int i = 0; i < uvs.Length; i += 4)
+            {
+                uvs[i + 0] = new Vector2(0f, 0f);
+                uvs[i + 1] = new Vector2(1f, 0f);
+                uvs[i + 2] = new Vector2(0f, 1f);
+                uvs[i + 3] = new Vector2(1f, 1f);
+            }
         }
+        else
+        {
+            int sectionCount = 0;
+            for (int i = 0; i < uvs.Length; i += 4)
+            {
+                uvs[i + 0] = new Vector2(0f, sectionCount / (float)points.Length);
+                uvs[i + 1] = new Vector2(1f, sectionCount / (float)points.Length);
+                uvs[i + 2] = new Vector2(0f,  (sectionCount + 1) / (float)points.Length);
+                uvs[i + 3] = new Vector2(1f, (sectionCount + 1) / (float)points.Length);
+                sectionCount++;
+            }
+        }
+
         return uvs;
     }
 }
